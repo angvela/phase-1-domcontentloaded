fa613258f82402d5972b67580742f5f021ec8386
angvela <angelamusamali05@gmali.com>
2025-07-01 10:34:28 +0300

    fix: ensure paragraph text is updated only if the element exists

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const paragraph = document.getElementById("text");
-  paragraph.textContent = "This is really cool!";
+  if (paragraph) {
+    paragraph.textContent = "This is really cool!";
+  }
 });
-
